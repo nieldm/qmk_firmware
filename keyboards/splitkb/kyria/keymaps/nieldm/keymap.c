edf2786038de434148cdd0dfc6d4002743a8b963
Daniel Mendez <daniel.mendez@team.bumble.com>
2023-01-26 10:57:12 +0100

    Update old key definition

--- keyboards/splitkb/kyria/keymaps/nieldm/keymap.c
+++ keyboards/splitkb/kyria/keymaps/nieldm/keymap.c
@@ -104,7 +104,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_NAV] = LAYOUT(
       _______ ,  KC_KP_1 ,   KC_KP_2 ,   KC_KP_3 ,  KC_KP_4 ,  KC_KP_5,                                   KC_PGUP, KC_HOME, KC_UP,   KC_END,  KC_VOLU, KC_DEL,
       _______ ,  KC_LCTL, KC_LALT, KC_LGUI, KC_LSFT,  REPEAT ,                                     KC_PGDN, KC_LEFT, KC_DOWN, KC_RGHT, KC_VOLD, KC_INS,
-      _______ ,  KC_KP_6 ,  KC_KP_7 ,KC_KP_8,KC_KP_9,KC_KP_0,KC_KP_DOT ,KC_SLCK, _______, _______,KC_PAUSE, KC_MPRV, KC_MPLY, KC_MNXT, KC_MUTE, KC_PSCR,
+      _______ ,  KC_KP_6 ,  KC_KP_7 ,KC_KP_8,KC_KP_9,KC_KP_0,KC_KP_DOT ,KC_SCRL, _______, _______,KC_PAUSE, KC_MPRV, KC_MPLY, KC_MNXT, KC_MUTE, KC_PSCR,
                                  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
     ),
 
